a3baed2fda0a7a66f9101b63716b086eb09d12bb
Morselli <vinicius_morselli@hotmail.com>
2022-04-07 15:25:24 -0300

    second commit

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,9 +6,9 @@ export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    username: "root",
+    password: "admin",
+    database: "workspace-backend",
     logging: true,
     entities: ["./src/entity/*.ts"],
     migrations: ["./src/migrations/*.ts"]
