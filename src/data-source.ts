5c4c617ea6b272650ccf3417f7ec37631b2ce2d8
Morselli <vinicius_morselli@hotmail.com>
2022-04-07 15:21:27 -0300

    first commit

---
+++ src/data-source.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "test",
+    password: "test",
+    database: "test",
+    logging: true,
+    entities: ["./src/entity/*.ts"],
+    migrations: ["./src/migrations/*.ts"]
+})
